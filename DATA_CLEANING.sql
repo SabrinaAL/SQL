cf2b2a180bd36fc88cdede1333989d7ea1e1de98
Sabrina <aliyeva.sabrina@gmail.com>
2020-06-22 00:16:39 -0700

    Updating DATA CLEANING methods in SQL

--- DATA_CLEANING.sql
+++ DATA_CLEANING.sql
@@ -55,3 +55,29 @@ FROM t1;
 
 SELECT date orig_date, (SUBSTR(date, 7, 4) || '-' || LEFT(date, 2) || '-' || SUBSTR(date, 4, 2))::DATE new_date
 FROM sf_crime_data;
+
+
+-- Use the accounts table to create first and last name columns that hold the first and last names for the primary_poc.
+SELECT LEFT(primary_poc, (POSITION(' ' IN primary_poc))) first_name, RIGHT(primary_poc, (LENGTH(primary_poc) -POSITION(' ' IN primary_poc))) last_name
+FROM accounts
+
+
+-- FILLING empty columns with sf_crime_data
+SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, o.*
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id
+WHERE o.total IS NULL;
+-------------------------------------------------------------
+
+SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, COALESCE(o.account_id, a.id) account_id, o.occurred_at, o.standard_qty, o.gloss_qty, o.poster_qty, o.total, o.standard_amt_usd, o.gloss_amt_usd, o.poster_amt_usd, o.total_amt_usd
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id
+WHERE o.total IS NULL;
+----------------------------------------------------------------
+SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, COALESCE(o.account_id, a.id) account_id, o.occurred_at, COALESCE(o.standard_qty, 0) standard_qty ,COALESCE(o.gloss_qty, 0) gloss_qty ,COALESCE(o.poster_qty, 0) poster_qty , o.total, o.standard_amt_usd, o.gloss_amt_usd, o.poster_amt_usd, o.total_amt_usd
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id
+WHERE o.total IS NULL;
