d005531a3a7fc5523b9b20acbdad9cdc6de12150
Sabrina <aliyeva.sabrina@gmail.com>
2020-06-30 22:49:13 -0700

    Updating Data Definition Language in Postgres

--- DDL.sql
+++ DDL.sql
@@ -168,3 +168,104 @@ UPDATE "user_data" SET "state_id" = (
 );
 
 ALTER TABLE "user_data" DROP COLUMN "state";
+
+-- Exercise Instructions
+-- For this exercise, you're going to have to explore the data schema in the Postgres workspace in order to determine which pieces of data require Unique and Primary Key constraints. Then, you'll have to execute the appropriate ALTER TABLE statements to add these constraints to the data set.
+
+-- Hint: There are 6 total constraints to be added.
+
+ALTER TABLE "books" ADD PRIMARY KEY ("id");
+
+ALTER TABLE "books" ADD UNIQUE ("isbn");
+
+ALTER TABLE "authors" ADD PRIMARY KEY ("id");
+
+ALTER TABLE "authors" ADD UNIQUE ("email_address");
+
+ALTER TABLE "book_authors" ADD PRIMARY KEY ("book_id", "author_id");
+
+ALTER TABLE "book_authors" ADD UNIQUE ("book_id", "contribution_rank");
+
+
+-- Exercise Instructions
+-- For this exercise, you're going to add some foreign key constraints to an existing schema, but you'll have to respect some business rules that were put in place:
+
+-- As a first step, please explore the currently provided schema and understand the relationships between all the tables
+-- Once that's done, please create all the foreign key constraints that are necessary to keep the referential integrity of the schema, with the following in mind:
+-- When an employee who's a manager gets deleted from the system, we want to keep all the employees that were under him/her. They simply won't have a manager assigned to them.
+-- We can't delete an employee as long as they have projects assigned to them
+-- When a project gets deleted from the system, we won't need to keep track of the people who were working on it.
+
+ALTER TABLE "employees"
+  ADD CONSTRAINT "valid_manager"
+  FOREIGN KEY ("manager_id") REFERENCES "employees" ("id") ON DELETE SET NULL;
+
+ALTER TABLE "employee_projects"
+  ADD CONSTRAINT "valid_employee"
+  FOREIGN KEY ("employee_id") REFERENCES "employees" ("id");
+
+ALTER TABLE "employee_projects"
+  ADD CONSTRAINT "valid_project"
+  FOREIGN KEY ("project_id") REFERENCES "projects" ("id") ON DELETE CASCADE;
+
+
+  -- Given a table users with a date_of_birth column of type DATE, write the SQL to add a requirement for users to be at least 18 years old.
+
+  ALTER TABLE "users"
+  ADD CONSTRAINT "users_must_be_over_18" CHECK (
+    CURRENT_TIMESTAMP - "date_of_birth" > INTERVAL '18 years'
+  );
+
+
+
+
+--   Exercise Instructions
+-- In this exercise, you're going to manage a database schema that contains no constraints, allowing you to practice all the concepts that you learned in this lesson!
+
+-- After exploring the schema, this is what you'll have to identify the following for each table, and add the appropriate constraints for them:
+
+-- Identify the primary key for each table
+-- Identify the unique constraints necessary for each table
+-- Identify the foreign key constraints necessary for each table
+-- In addition to the three types of constraints above, you'll have to implement some custom business rules:
+-- Usernames need to have a minimum of 5 characters
+-- A book's name cannot be empty
+-- A book's name must start with a capital letter
+-- A user's book preferences have to be distinct
+
+  -- Primary and unique keys
+ALTER TABLE "users"
+  ADD PRIMARY KEY ("id"),
+  ADD UNIQUE ("username"),
+  ADD UNIQUE ("email");
+
+ALTER TABLE "books"
+  ADD PRIMARY KEY ("id"),
+  ADD UNIQUE ("isbn");
+
+ALTER TABLE "user_book_preferences"
+  ADD PRIMARY KEY ("user_id", "book_id");
+
+
+-- Foreign keys
+ALTER TABLE "user_book_preferences"
+  ADD FOREIGN KEY ("user_id") REFERENCES "users",
+  ADD FOREIGN KEY ("book_id") REFERENCES "books";
+
+
+-- Usernames need to have a minimum of 5 characters
+ALTER TABLE "users" ADD CHECK (LENGTH("username") >= 5);
+
+
+-- A book's name cannot be empty
+ALTER TABLE "books" ADD CHECK(LENGTH(TRIM("name")) > 0);
+
+
+-- A book's name must start with a capital letter
+ALTER TABLE "books" ADD CHECK (
+  SUBSTR("name", 1, 1) = UPPER(SUBSTR("name", 1, 1))
+);
+
+
+-- A user's book preferences have to be distinct
+ALTER TABLE "user_book_preferences" ADD UNIQUE ("user_id", "preference");
